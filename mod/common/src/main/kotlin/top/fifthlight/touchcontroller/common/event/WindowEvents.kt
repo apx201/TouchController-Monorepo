a8c096b474b9702dd2ed7ee98709e78bb8b2b2c4
fifth_light <liu-yu-jie@foxmail.com>
2025-07-28 19:29:34 +0800

    添加对 Ixeris 的兼容

--- mod/common/src/main/kotlin/top/fifthlight/touchcontroller/common/event/WindowEvents.kt
+++ mod/common/src/main/kotlin/top/fifthlight/touchcontroller/common/event/WindowEvents.kt
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory
 import top.fifthlight.touchcontroller.common.gal.PlatformWindowProvider
 import top.fifthlight.touchcontroller.common.platform.PlatformProvider
 import top.fifthlight.touchcontroller.proxy.message.InitializeMessage
+import java.util.concurrent.Executor
 
 object WindowEvents : KoinComponent {
     private val logger = LoggerFactory.getLogger(WindowEvents::class.java)
@@ -16,10 +17,39 @@ object WindowEvents : KoinComponent {
     val windowHeight: Int
         get() = windowProvider.windowHeight
 
+    private fun interface MainThreadDispatcher : Executor
+
+    private class EmptyDispatcher : MainThreadDispatcher {
+        override fun execute(command: Runnable) = command.run()
+    }
+
+    private class IxerisDispatcher(clazz: Class<*>) : MainThreadDispatcher {
+        init {
+            require(clazz.canonicalName == "me.decce.ixeris.api.IxerisApi") { "Bad ixeris API class" }
+        }
+
+        private val instance = clazz.getMethod("getInstance").invoke(null)
+        private val runLaterOnMainThreadMethod = clazz.getMethod("runLaterOnMainThread", Runnable::class.java)
+
+        override fun execute(command: Runnable) {
+            runLaterOnMainThreadMethod.invoke(instance, command)
+        }
+    }
+
+    private val mainThreadDispatcher by lazy {
+        try {
+            IxerisDispatcher(Class.forName("me.decce.ixeris.api.IxerisApi"))
+        } catch (_: ClassNotFoundException) {
+            EmptyDispatcher()
+        }
+    }
+
     fun onWindowCreated(windowProvider: PlatformWindowProvider) {
         this.windowProvider = windowProvider
-        platformProvider.load(windowProvider)
-        platformProvider.platform?.sendEvent(InitializeMessage)
-        logger.info("Loaded platform")
+        mainThreadDispatcher.execute {
+            platformProvider.load(windowProvider)
+            platformProvider.platform?.sendEvent(InitializeMessage)
+            logger.info("Loaded platform on thread ${Thread.currentThread().name}")
+        }
     }
 }
